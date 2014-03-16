3cae1c13b051c1366c0a3deab6ca8cfde18a4a53
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-16 03:37:14 -0300

    Fix travis env variables

--- travis/env.sh
+++ travis/env.sh
@@ -2,10 +2,8 @@
 
 # This script is to be *sourced* by the other scripts to set up env variables
 
-if [ "$TRAVIS" == "true" ]; then
-    export IVY_REPO_DIR="$HOME/ivy_repo"
-    export TOOLCHAINS_DIR="$HOME/toolchains"
-    export PATH=$PATH:$TOOLCHAINS_DIR/gcc-4.8.1-for-linux64/bin:$TOOLCHAINS_DIR/arm-linux-androideabi-gcc-4.8-android-14/bin
+export IVY_REPO_DIR="$HOME/ivy_repo"
+export TOOLCHAINS_DIR="$HOME/toolchains"
+export PATH=$PATH:$TOOLCHAINS_DIR/gcc-4.8.1-for-linux64/bin:$TOOLCHAINS_DIR/arm-linux-androideabi-gcc-4.8-android-14/bin
 
-    GIT_CREDENTIALS="$HOME/.git_credentials"
-fi
+GIT_CREDENTIALS="$HOME/.git_credentials"
