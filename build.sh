2ccf43c7f5dd2c06a258c716225c72ba9bf5a2b7
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-16 03:57:56 -0300

    Travis fix + diagnostics

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -x
 set -e
 
-env
-
 [ -z "$TRAVIS_COMMIT" ] && source travis/env.sh
+
+env
 
 for p in poco sqlite3 libodb libodb-sqlite gtest gmock; do
     ./gradlew --no-color ${p}:publish --info
