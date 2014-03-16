e12f68246b8aa5c9c6f97dff8a90878a02be9efe
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-16 03:32:29 -0300

    Fix travis env variables

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+[ "$TRAVIS" == "true" ] && source travis/env.sh
+
 for p in poco sqlite3 libodb libodb-sqlite gtest gmock; do
     ./gradlew --no-color ${p}:publish --info
 done
--- travis/env.sh
+++ travis/env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# This script is to be sourced by the other scripts to set up env variables
+# This script is to be *sourced* by the other scripts to set up env variables
 
-export IVY_REPO_DIR="$HOME/ivy_repo"
-export TOOLCHAINS_DIR="$HOME/toolchains"
-export PATH=$PATH:$TOOLCHAINS_DIR/gcc-4.8.1-for-linux64/bin:$TOOLCHAINS_DIR/arm-linux-androideabi-gcc-4.8-android-14/bin
+if [ "$TRAVIS" == "true" ]; then
+    export IVY_REPO_DIR="$HOME/ivy_repo"
+    export TOOLCHAINS_DIR="$HOME/toolchains"
+    export PATH=$PATH:$TOOLCHAINS_DIR/gcc-4.8.1-for-linux64/bin:$TOOLCHAINS_DIR/arm-linux-androideabi-gcc-4.8-android-14/bin
 
-GIT_CREDENTIALS="$HOME/.git_credentials"
+    GIT_CREDENTIALS="$HOME/.git_credentials"
+fi
