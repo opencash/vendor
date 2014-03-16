72857dcfcc2512063c7a7b99fbe871bdb26b6053
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-16 20:25:57 -0300

    More verbose commit messages for ivy_repo

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     ./update-directory-index.sh
     git add -f .
-    git commit -m "Travis-CI: build $TRAVIS_REPO_SLUG #$TRAVIS_BUILD_NUMBER"
+    git commit -m "Travis-CI: build $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH #$TRAVIS_BUILD_NUMBER ($TRAVIS_COMMIT, $TRAVIS_COMMIT_RANGE)"
     git push -fq origin gh-pages
     echo "done"
 fi
