2868f8d27ef301a03f7b3b2bf77f7b76cf988b1b
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-04-20 14:36:38 -0300

    Add more verbosity, otherwise travis job gets killed

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 [ "$TRAVIS_COMMIT" ] && source travis/env.sh
 
 GRADLE_FLAGS=
-[ "$TRAVIS_COMMIT" ] && GRADLE_FLAGS='--no-color -q'
+[ "$TRAVIS_COMMIT" ] && GRADLE_FLAGS='--no-color'
 
 for p in poco sqlite3 libodb libodb-sqlite gtest gmock; do
     ./gradlew $GRADLE_FLAGS ${p}:publish
